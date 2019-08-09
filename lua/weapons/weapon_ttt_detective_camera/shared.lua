52f9948cf6bbb8a6ce0da1a00296e96b3a83b9c7
Michael Hawkins <BadgerCode@users.noreply.github.com>
2019-08-09 10:06:00 +0100

    initial files

---
+++ lua/weapons/weapon_ttt_detective_camera/shared.lua
@@ -0,0 +1,11 @@
+SWEP.Base = "weapon_tttbase"
+
+SWEP.HoldType = "normal"
+
+SWEP.ViewModel  = "models/weapons/v_crowbar.mdl"
+SWEP.WorldModel = "models/props/cs_office/Cardboard_box02.mdl"
+
+SWEP.Kind = WEAPON_EQUIP2
+SWEP.AutoSpawnable = false
+SWEP.CanBuy = { ROLE_DETECTIVE }
+SWEP.LimitedStock = true
